03b8bc561a2f92217399a1b8bfe20a6c62c759d8
Kunting <hellokunting@gmail.com>
2021-05-16 21:45:42 -0700

    Update

--- users_app/views.py
+++ users_app/views.py
@@ -4,13 +4,12 @@ from django.contrib import messages
 
 # Create your views here.
 def register(request):
-  if request.method=="POST":
-    register_form = CustomRegisterForm(request.POST)
-    if register_form.is_valid():
-      register_form.save()
-      messages.success(request, ("New User Account Created! Please Log in to get started."))
-      return redirect('register')
-  else: 
-    register_form = CustomRegisterForm()
+    if request.method=="POST":
+        register_form = CustomRegisterForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, ("New User Account Created, Login To Get Started!"))
+            return redirect('register')
+    else:
+        register_form = CustomRegisterForm()
     return render(request, 'register.html', {'register_form': register_form})
-
